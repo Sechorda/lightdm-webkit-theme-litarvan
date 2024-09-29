ae0e461003ce5741faba4d4423ceff848713643f
sechorda <sechorda@gmail.com>
2024-09-29 11:28:07 -0400

    renaming

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo Building LightDM Webkit theme Litarvan-min v$(cat version)...
+echo Building LightDM Webkit theme Sechorda v$(cat version)...
 echo
 
-rm -f lightdm-webkit-theme-litarvan-min-$(cat version).tar.gz
+rm -f lightdm-webkit-theme-sechorda-$(cat version).tar.gz
 npm ci
 rm -rf dist/
 npm run-script build
 cp LICENSE dist/
-pushd dist && tar zcvf ../lightdm-webkit-theme-litarvan-min-$(cat ../version).tar.gz ./* && popd
+pushd dist && tar zcvf ../lightdm-webkit-theme-sechorda-$(cat ../version).tar.gz ./* && popd
